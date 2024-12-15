c35526ffd19170d4dbce184a9e01ddd7d752d718
DavidCastro21 <up202108763@up.pt>
2024-12-15 19:24:01 +0000

    add processing query

---
+++ pri-ui/backend/utils.js
@@ -0,0 +1,28 @@
+import natural from 'natural';
+import stopword from 'stopword';
+
+const tokenizer = new natural.WordTokenizer();
+const language = "EN"
+const defaultCategory = 'N';
+const defaultCategoryCapitalized = 'NNP';
+
+const lexicon = new natural.Lexicon(language, defaultCategory, defaultCategoryCapitalized);
+const ruleSet = new natural.RuleSet('EN');
+const tagger = new natural.BrillPOSTagger(lexicon, ruleSet);
+
+function preprocessQuery(query) {
+  let tokens = tokenizer.tokenize(query);
+
+  tokens = stopword.removeStopwords(tokens);
+
+  const taggedTokens = tagger.tag(tokens);
+
+  const filteredTokens = taggedTokens.taggedWords.filter(token => 
+    token.tag.startsWith('N') || token.tag.startsWith('JJ')
+  ).map(token => token.token);
+
+  return filteredTokens.join(" ");
+}
+
+export { preprocessQuery };
+
